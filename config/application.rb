00ca1c8cd31163ee48f9872b735e0b88f8520ac0
Diogo Azvg <diogo.azvg@gmail.com>
2018-05-03 11:03:54 -0300

    secrets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module GestaoDeRotasEVeiculos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.i18n.default_locale = :"pt-BR"
+    config.time_zone = 'Brasilia'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
